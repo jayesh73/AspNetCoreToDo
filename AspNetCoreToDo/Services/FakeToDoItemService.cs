ee3771dd8d7d5e895b24271a4a48e1c16cc7bdc8
jayesh73 <patelj@gpcl.com.au>
2021-09-18 18:21:30 +1000

    Add controller

---
+++ AspNetCoreToDo/Services/FakeToDoItemService.cs
@@ -0,0 +1,33 @@
+﻿using AspNetCoreToDo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreToDo.Services
+{
+    public class FakeToDoItemService: IToDoItemService
+    {
+        public Task<IEnumerable<ToDoItem>> GetIncompleteItemsAsync()
+        {
+            IEnumerable<ToDoItem> items = new[]
+            {
+                new ToDoItem
+                {
+                    Title = "Learn Asp .Net Core",
+                    DueAt = DateTimeOffset.Now.AddDays(1)
+                },
+                new ToDoItem
+                {
+                    Title= "Build awesome apps",
+                    DueAt = DateTimeOffset.Now.AddDays(2)
+                }
+            };
+
+            return Task.FromResult(items);
+            
+            // throw new NotImplementedException();
+        }
+        
+    }
+}
